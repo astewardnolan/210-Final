024ae0f7307538a2a88c51e4d605650c7983ff68
astewardnolan <astewardnolan@smith.edu>
2023-12-08 16:32:00 -0500

    tried to get gradle to work

---
+++ finalproj/app/src/main/java/finalproj/Main.java
@@ -0,0 +1,38 @@
+package finalproj;
+import java.io.*;
+import java.util.*;
+import com.google.common.graph.*;
+
+class Main{
+
+    static HashSet<Station> allStations = new HashSet<Station>();
+    MutableValueGraph<Integer, Double> weightedGraph = ValueGraphBuilder.directed().build();
+
+
+    public static void main (String args[]){
+
+        Scanner file = null;
+        System.out.println("amtrack.txt");
+        try {
+            file = new Scanner(new File("data/amtrack.txt"));
+            
+        }catch(FileNotFoundException e){
+            System.err.println("Cannot locate file.");
+            System.exit(-1);
+        }
+        file.nextLine();
+
+        while(file.hasNextLine()){
+            String[] data = file.nextLine().split(",");
+            allStations.add(new Station(Double.parseDouble(data[0]),Double.parseDouble(data[1]),data[3],data[4],data[5]));
+        }
+        
+        System.out.println(allStations);
+
+
+
+    }
+
+
+
+}
---
+++ finalproj/app/src/main/java/finalproj/Station.java
@@ -0,0 +1,23 @@
+package finalproj;
+
+public class Station {
+
+    double xCord;
+    double yCord;
+    String prevStation;
+    String destinationStation;
+    String nextStation;
+    
+    public Station (double x, double y, String from, String current, String next){
+        xCord=x;
+        yCord=y;
+        prevStation=from;
+        destinationStation = current;
+        nextStation = next;
+    }
+
+    public String toString(){
+        return "X cord is: "+xCord+", y cord is: "+yCord+", train is coming from "+prevStation+" and going to "+destinationStation;
+    }
+
+}
