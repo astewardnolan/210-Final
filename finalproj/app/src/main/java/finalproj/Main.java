a5efc5ac827cfb6967a79326da8f715b46659c9f
astewardnolan <astewardnolan@smith.edu>
2023-12-10 19:48:22 -0500

    okay played around w graph

--- finalproj/app/src/main/java/finalproj/Main.java
+++ finalproj/app/src/main/java/finalproj/Main.java
@@ -6,11 +6,12 @@ import com.google.common.graph.*;
 class Main{
 
     static HashSet<Station> allStations = new HashSet<Station>();
-    MutableValueGraph<Integer, Double> weightedGraph = ValueGraphBuilder.directed().build();
-
+    //MutableValueGraph<Integer, Double> weightedGraph = ValueGraphBuilder.directed().build();
 
     public static void main (String args[]){
 
+        MutableValueGraph<Integer, Double> weightedGraph = ValueGraphBuilder.directed().build();
+        
         Scanner file = null;
         System.out.println("amtrack.txt");
         try {
@@ -30,12 +31,20 @@ class Main{
             
             if(data[0].length()>0 )  {
                 allStations.add(new Station(Double.parseDouble(data[0]),Double.parseDouble(data[1]),data[3],data[4],data[5]));
-            }
-         
+            }  
     }
-        
         System.out.println(allStations);
 
+        weightedGraph.addNode(3);
+        weightedGraph.addNode(2);
+        
+        System.out.println(weightedGraph);
+
+        //graph building stuff here!
+        for(Station station: allStations){
+            //weightedGraph.addNode(destinationStation);
+            
+        }
 
 
     }
